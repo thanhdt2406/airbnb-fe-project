ab5aac716d0c282e12e0d2f4a671cd7474136a39
votiendung <votiendung.92@gmail.com>
2021-01-16 12:39:40 +0700

    edit user profile

--- src/app/component/user-profile/user-profile.component.ts
+++ src/app/component/user-profile/user-profile.component.ts
@@ -1,28 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../service/user/user.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../../service/auth/auth.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../model/user';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
 declare var $: any;
+declare var Swal: any;
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-  userProfileForm: FormGroup = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('',[Validators.required, Validators.email]),
-    phone_number: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(11)]),
-    avatar: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required)
-  });
-
+  imgSrc: string = '';
   id: number = -1;
+  selectedImages: any = null;
   currentUser = this.authService.currentUserValue;
+  userProfileForm: FormGroup = new FormGroup({
+    name: new FormControl(this.currentUser.name, Validators.required),
+    email: new FormControl(this.currentUser.email,[Validators.required, Validators.email]),
+    phone_number: new FormControl(this.currentUser.phone_number, [Validators.required, Validators.minLength(10), Validators.maxLength(11)]),
+    avatar: new FormControl(this.currentUser.avatar, Validators.required),
+    address: new FormControl(this.currentUser.address, Validators.required)
+  });
   constructor(private userService: UserService,
               private activatedRoute: ActivatedRoute,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private storage: AngularFireStorage,
+              private router: Router) { }
 
   ngOnInit(): void {
     $(document).ready(function() {
@@ -72,15 +79,51 @@ export class UserProfileComponent implements OnInit {
       });
 
     });
-
-    this.activatedRoute.paramMap.subscribe(paramMap => {
-      // @ts-ignore
-      this.id = +paramMap.get('id');
-    });
+    this.currentUser;
   }
 
   getUserById(id: number) {
     return this.userService.getUserById(id).toPromise();
   }
+
+  updateUserProfile(id: number) {
+    const userProfile: User = {
+      id: this.currentUser.id,
+      name: this.userProfileForm.value.name === "" ? this.currentUser.name : this.userProfileForm.value.name,
+      email: this.userProfileForm.value.email === "" ? this.currentUser.email : this.userProfileForm.value.email,
+      phone_number: this.userProfileForm.value.phone_number === "" ? this.currentUser.phone_number : this.userProfileForm.value.phone_number,
+      address: this.userProfileForm.value.address === "" ? this.currentUser.address : this.userProfileForm.value.address,
+    }
+    this.userService.updateUserById(id, userProfile).toPromise();
+
+  }
+  submit() {
+    if (this.selectedImages !== null) {
+      const filePath = `${this.currentUser.user_name}/${this.selectedImages.name.split('.').slice(0, -1).join('.')}_${new Date().getTime()}`;
+      const fileRef = this.storage.ref(filePath);
+      this.storage.upload(filePath, this.selectedImages).snapshotChanges().pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe(async url => {
+            this.currentUser.avatar = url;
+            // @ts-ignore
+            await this.updateUserProfile(this.currentUser.id);
+            alert("Success!");
+            this.router.navigate(["/"]);
+          });
+        })
+      ).subscribe();
+    }
+  }
+
+  showPreview(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImages = event.target.files[0];
+    } else {
+      this.selectedImages = null;
+    }
+  }
+
 
 }
